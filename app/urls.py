053ee287bea5d57c22845ecd9194f808667d777f
Ksu <k.kumova@mail.ru>
2024-04-10 12:55:24 +0300

    fixed components

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('settings/', views.settings, name='settings'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('ask/', views.ask, name='ask'),
     path('questions/<int:question_id>', views.question, name='question'),
 ]
--- app/views.py
+++ app/views.py
@@ -14,44 +14,36 @@ ANSWERS = [
         "id": i,
         "title": f"Answer {i}",
         "text": f"This is answer number {i}"
-    } for i in range(10)
+    } for i in range(20)
 ]
 
 
-
-def index(request):
+def pagination(request, data):
     page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
+    paginator = Paginator(data, 5)
     try:
-        if int(page_num) > paginator.num_pages or int(page_num)<0:
+        if int(page_num) > paginator.num_pages or int(page_num) < 0:
             page_num = paginator.num_pages
     except:
         page_num = paginator.num_pages
-    page_obj = paginator.page(page_num)
+    return paginator.page(page_num)
+
+
+def index(request):
+    page_obj = pagination(request, QUESTIONS)
     return render(request, "index.html", {"questions": page_obj})
 
 
 def hot(request):
-    questions = QUESTIONS[10:]
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(questions, 5)
-    try:
-        if int(page_num) > paginator.num_pages or int(page_num)<0:
-            page_num = paginator.num_pages
-    except:
-        page_num = paginator.num_pages
-    page_obj = paginator.page(page_num)
+    page_obj = pagination(request, QUESTIONS[:10])
     return render(request, "hot.html", {"questions": page_obj})
 
 
 def question(request, question_id):
     if question_id > len(QUESTIONS):
-        question_id = len(QUESTIONS)-1
+        question_id = len(QUESTIONS) - 1
     item = QUESTIONS[question_id]
-    answers = ANSWERS[5:]
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(answers, 3)
-    page_obj = paginator.page(page_num)
+    page_obj = pagination(request, ANSWERS[10:])
     return render(request, "question.html", {"question": item, "answers": page_obj})
 
 
@@ -62,7 +54,12 @@ def login(request):
 def signup(request):
     return render(request, "signup.html")
 
+
 def ask(request):
     return render(request, "ask.html")
+
+
 def settings(request):
     return render(request, "settings.html")
+def ask(request):
+    return render(request, "ask.html")
