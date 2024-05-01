ba84068c6e07c67affccffc82747157269c55251
Ksu <k.kumova@mail.ru>
2024-05-01 09:01:51 +0300

    task 6

--- askme_pupkin/wsgi.py
+++ askme_pupkin/wsgi.py
@@ -14,3 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_pupkin.settings')
 
 application = get_wsgi_application()
+
+
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+
+    params = environ.get('QUERY_STRING', '')
+    params_list = params.split('&')
+    output = '\n'.join(params_list)
+
+    return [output.encode()]
---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+command = '/home/ksu/technopark/askme_Kumova/askme_venv/bin/gunicorn'
+pythonpath = '/home/ksu/technopark/askme_Kumova'
+bind = 'localhost:8000'
+workers = 10
+timeout = 600
